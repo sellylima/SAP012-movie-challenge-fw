ece82898a9498bb1aa8b965474680b9bf1aa8908
sellylima <vanessa.limanasc@gmail.com>
2024-06-25 23:46:52 -0300

    first file

--- cinemax/src/app/app.module.ts
+++ cinemax/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './layout/movie-card/movie-card.component';
+import { ApiService } from './shared/api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CommonModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
