d7e45e0fb537cc4196bcc2410ec110db116228bf
sellylima <vanessa.limanasc@gmail.com>
2024-06-25 22:07:36 -0300

    add type model typescript

---
+++ cinemax/src/app/models/Movie.d.ts
@@ -0,0 +1,9 @@
+export type Movie = {
+    title: string;
+    poster_path: string;
+    release_year: number;
+    id: number;
+    overview: string;
+    // vote_average: number;
+    genres: string[];
+}
