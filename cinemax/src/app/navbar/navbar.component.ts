706de57026de2af795b0143bd96a447e87ac0f6e
sellylima <vanessa.limanasc@gmail.com>
2024-08-20 00:46:28 -0300

    adjustments

--- cinemax/src/app/navbar/navbar.component.ts
+++ cinemax/src/app/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  navbg: any;
 
+  @HostListener('document:scroll') scrollver() {
+    console.log(document.body.scrollTop, 'scrolllenght#');
+
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.navbg = {
+        'background-color': '#1A2232',
+      };
+    } else {
+      this.navbg = {};
+    }
+  }
 }
